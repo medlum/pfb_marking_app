964cc3d7e0cbb5bb44e58859a2a8b7922f52c26d
medlum <andy_oh18@yahoo.com>
2026-02-06 12:58:53 +0800

    update qwen2.5

--- utils_inference.py
+++ utils_inference.py
@@ -17,7 +17,7 @@ def initialize_inferenceclient():
 model_list = [  "meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
                 "Qwen/Qwen3-235B-A22B-Instruct-2507-tput",
                 #"meta-llama/Llama-3.3-70B-Instruct-Turbo",
-                #"Qwen/Qwen2.5-72B-Instruct-Turbo",
+                "Qwen/Qwen2.5-72B-Instruct-Turbo",
                 "openai/gpt-oss-120b",
                 "openai/gpt-oss-20b",
                 ""  
